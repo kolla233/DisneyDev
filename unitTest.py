f372dc5a2fb3c0ba2a84ada6c1dd53d867a2e507
Kolla <rachowdary@deloitte.com>
2020-03-18 11:26:28 +0530

    Added files

---
+++ unitTest.py
@@ -0,0 +1,19 @@
+import unittest
+import configparser
+config = configparser.ConfigParser()
+config.read("C:\\Users\\rachowdary\\Downloads\\JSON_Input_Files\\abc.txt")
+a = config.get("addition", "a")
+b = config.get("addition", "b")
+result= config.get("addition", "result")
+num1 = int(a)
+num2 = int(b)
+result = int(result)
+def add(num1,num2):
+    return num1 + num2
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(add(num1,num2), result)
+
+if __name__ == '__main__':
+    unittest.main()
